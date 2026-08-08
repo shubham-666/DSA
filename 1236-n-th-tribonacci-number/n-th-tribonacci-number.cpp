bc88212b7775c210374645d4bf06263effc9c535
SHUBHAM TIWARI <98177201+shubham-666@users.noreply.github.com>
2026-08-08 14:03:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (24.28%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -0,0 +1,18 @@
+// Tabulation
+// Time Complexity: O(N)
+// Space Complexity: O(N)
+class Solution {
+public:
+    int tribonacci(int n) {
+        vector<int> dp(n+3, 0);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for(int i=3;i<=n;i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+
+        return dp[n];
+    }
+};
