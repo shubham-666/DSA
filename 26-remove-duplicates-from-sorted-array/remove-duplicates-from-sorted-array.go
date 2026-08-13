6a3d36c1545122d09d7ee9df56a35fdba7782f1d
SHUBHAM TIWARI <98177201+shubham-666@users.noreply.github.com>
2026-08-13 12:47:17 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (43.71%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.go
@@ -0,0 +1,14 @@
+func removeDuplicates(nums []int) int {
+    if len(nums) == 0 {
+        return 0
+    }
+    k := 1
+
+    for i := 1; i < len(nums); i ++ {
+        if nums[i] != nums[i-1] {
+            nums[k] = nums[i]
+            k++
+        }
+    }
+    return k
+}
