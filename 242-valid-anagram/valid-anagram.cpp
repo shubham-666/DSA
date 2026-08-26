3e3887a6eeb3788ae5a75675ef107a9f34eaaffb
SHUBHAM TIWARI <98177201+shubham-666@users.noreply.github.com>
2026-08-26 22:24:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (75.72%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) {
+            return false;
+        }
+
+        vector<int> count(26, 0);
+
+        for (int i = 0; i < s.size(); i++) {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }
+
+        for (int value : count) {
+            if (value != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
