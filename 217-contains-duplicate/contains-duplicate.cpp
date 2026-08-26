1b79cca8a9568666398437564daa2d9703a32e1c
SHUBHAM TIWARI <98177201+shubham-666@users.noreply.github.com>
2026-08-26 22:31:10 +0530

    Time: 75 ms (43.83%) | Memory: 111.2 MB (62.46%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -3,11 +3,12 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_set<int> seen;
 
-        for (int num : nums) {
-            if (seen.count(num)) {
+        for (int number : nums) {
+            if (seen.count(number)) {
                 return true;
             }
-            seen.insert(num);
+
+            seen.insert(number);
         }
 
         return false;
