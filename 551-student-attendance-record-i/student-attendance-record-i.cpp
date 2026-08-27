2bf3f5f0e907e6f38f8b728897bcdf4bdc93ff6a
SHUBHAM TIWARI <98177201+shubham-666@users.noreply.github.com>
2026-08-27 20:58:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (15.61%) - LeetSync

---
+++ 551-student-attendance-record-i/student-attendance-record-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int absent = 0;
+        int late = 0;
+
+        for (char ch : s) {
+            if (ch == 'A') {
+                absent++;
+                late = 0;
+            } 
+            else if (ch == 'L') {
+                late++;
+                if (late >= 3) {
+                    return false;
+                }
+            } 
+            else {
+                late = 0;
+            }
+
+            if (absent >= 2) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
