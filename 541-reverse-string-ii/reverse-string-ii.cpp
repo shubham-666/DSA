86a269a2ef33738052190a9c293c0a982d35241d
SHUBHAM TIWARI <98177201+shubham-666@users.noreply.github.com>
2026-08-27 20:57:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (77.45%) - LeetSync

---
+++ 541-reverse-string-ii/reverse-string-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        for (int i = 0; i < s.size(); i += 2 * k) {
+            int right = min(i + k, (int)s.size());
+
+            reverse(s.begin() + i, s.begin() + right);
+        }
+
+        return s;
+    }
+};
