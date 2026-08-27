27dcf12ef9a573876c3a599735f8b4c0ca1da414
SHUBHAM TIWARI <98177201+shubham-666@users.noreply.github.com>
2026-08-27 21:02:16 +0530

    Time: 8 ms (90.36%) | Memory: 14.4 MB (92.68%) - LeetSync

---
+++ 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+
+        int depth = 0;
+
+        for (Node* child : root->children) {
+            depth = max(depth, maxDepth(child));
+        }
+
+        return depth + 1;
+    }
+};
