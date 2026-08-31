0dd685efdaa87128ba3d75881381d6f6bad915e0
SHUBHAM TIWARI <98177201+shubham-666@users.noreply.github.com>
2026-08-31 14:28:17 +0530

    Time: 15 ms (58.93%) | Memory: 19.3 MB (22.27%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.py
@@ -0,0 +1,16 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        x = 0
+        y = 0
+
+        for move in moves:
+            if move == 'R':
+                x += 1
+            elif move == 'L':
+                x -= 1
+            elif move == 'U':
+                y += 1
+            else:
+                y -= 1
+
+        return x == 0 and y == 0
